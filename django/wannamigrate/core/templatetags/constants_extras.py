3d99af906990f405f615130bad1c864ce1902c63
Marcio Oliveira <marciomjmo@gmail.comm>
2014-09-18 18:46:18 -0300

    templatetags centralized on the core app. \n Created the password recovery form on the signup popup.

--- django/wannamigrate/core/templatetags/constants_extras.py
+++ django/wannamigrate/core/templatetags/constants_extras.py
@@ -1,8 +1,10 @@
 """
-    Template utilities. To use the template methods defined here you should load this file
-    on the desired template using: {% load template_utilities %}
-"""
+    Constants extras. This file defines methods to handle constants, including access
+    to the constants file.
 
+    To use the template methods defined here you should load this module
+    on the desired template using: {% load constants_extras %}
+"""
 from django import template
 from wannamigrate import constants
 
@@ -17,4 +19,4 @@ def get_constant_value( name ):
         :param name: The name of the constant to retrieve.
         :return The value of the constant.
     """
-    return getattr( constants, name, "")
+    return getattr( constants, name, "" )
---
+++ django/wannamigrate/core/templatetags/dictionary_extras.py
@@ -0,0 +1,22 @@
+"""
+    Dictionary extras. This file defines methods to handle dictionaries.
+
+    To use the template methods defined here you should load this module
+    on the desired template using: {% load dictionary_extras %}
+"""
+from django import template
+
+register = template.Library()
+
+@register.filter( name = 'get_key' )
+def get_key(dict, key):
+    """
+        Returns the value of a key in a dictionary.
+
+        :param key: The key to be catched.
+        :return The value for the given key in the dictionary, or empty if not set.
+    """
+    try:
+        return dict[key]
+    except KeyError:
+        return ''
