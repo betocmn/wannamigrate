10d4454c86530804a5e3ea98e98b7fc0aa3fa943
Humberto Moreira <humberto@intertech.com.br>
2014-11-28 20:58:59 -0300

    Fixed bug on Work Form and Logo URL for email

--- django/wannamigrate/site/forms.py
+++ django/wannamigrate/site/forms.py
@@ -452,9 +452,11 @@ class UserWorkForm( BaseModelForm ):
         if 'user' in kwargs:
             self.user = kwargs.pop( "user" )
         super( UserWorkForm, self ).__init__( *args, **kwargs )
-        if self._raw_value( 'occupation_category' ) is not None:
+        if self._raw_value( 'occupation_category' ) is not None and self._raw_value( 'occupation_category' ) != '':
             self.fields['occupation_category'].initial = self._raw_value( 'occupation_category' )
             self.fields['occupation'].queryset = Occupation.objects.filter( occupation_category = self._raw_value( 'occupation_category' ) )
+        elif self.fields['occupation_category'].initial:
+            self.fields['occupation'].queryset = Occupation.objects.filter( occupation_category = self.fields['occupation_category'].initial )
         elif self.instance and self.instance.occupation and self.instance.occupation.occupation_category_id:
             self.fields['occupation_category'].initial = self.instance.occupation.occupation_category_id
             self.fields['occupation'].queryset = Occupation.objects.filter( occupation_category_id = self.instance.occupation.occupation_category_id )
