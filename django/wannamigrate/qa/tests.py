e1e05c8a44b86310bf0816cdba41951cf2da2e3f
Marcio Oliveira <marciomjmo@gmail.com>
2015-02-03 17:55:18 -0300

    Improved tests.py. Added blank param to possible empty texts and charfields on models.py

--- django/wannamigrate/qa/tests.py
+++ django/wannamigrate/qa/tests.py
@@ -1,10 +1,13 @@
 """
     QA Unit tests.
+    Remember that the tests creates a new table on the database, so the database user should have 
+    CREATE DATABASE privilegies.
 """
 
 ########################
 # Imports
 ########################
+from django.conf import settings
 from django.test import TestCase
 from wannamigrate.qa.models import Post
 
@@ -14,7 +17,7 @@ from wannamigrate.qa.models import Post
 #######################
 # Testing classes
 #######################
-class ModelingTester( TestCase ):
+class ModelingTest( TestCase ):
     """ 
         This class provides tests units to the database modeling.
     """
@@ -31,6 +34,18 @@ class ModelingTester( TestCase ):
         """
 
 
+    def test_constants_definitions( self ):
+        """
+            Tests if the required constants of the QA app was defined.
+        """
+
+        # Checks Post, Question, Answer and Comment id constants
+        self.assertIsNotNone( settings.QA_POST_TYPE_BLOGPOST_ID )
+        self.assertIsNotNone( settings.QA_POST_TYPE_QUESTION_ID )
+        self.assertIsNotNone( settings.QA_POST_TYPE_ANSWER_ID )
+        self.assertIsNotNone( settings.QA_POST_TYPE_COMMENT_ID )
+
+
     def test_integrity_correctness( self ):
         """
             Tests the relations of the QA models, ensuring that everything is ok.
@@ -39,3 +54,15 @@ class ModelingTester( TestCase ):
                 - A Comment and an Answer cannot have title.
                 - etc
         """
+
+        # A question should not have a parent
+        result = Post.objects.filter( post_type = settings.QA_POST_TYPE_QUESTION_ID, parent__isnull = False )
+        self.assertEqual( len( result ), 0 )
+
+        # A comment should not have a title
+        result = Post.objects.filter( post_type = settings.QA_POST_TYPE_COMMENT_ID, title != "" )
+        self.assertEqual( len( result ), 0 )
+
+        # An answer should not have a title
+        result = Post.objects.filter( post_type = settings.QA_POST_TYPE_ANSWER_ID, title != "" )
+        self.assertEqual( len( result ), 0 )
