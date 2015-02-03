35365ce1a7754ac9f03f3aa3f3f78d51dcdcebfa
Marcio Oliveira <marciomjmo@gmail.com>
2015-02-03 16:23:50 -0300

    Added a initial structure to the tests.py file insinde the QA app.

--- django/wannamigrate/qa/tests.py
+++ django/wannamigrate/qa/tests.py
@@ -1,3 +1,41 @@
-from django.test import TestCase
+"""
+    QA Unit tests.
+"""
 
-# Create your tests here.
+########################
+# Imports
+########################
+from django.test import TestCase
+from wannamigrate.qa.models import Post
+
+
+
+
+#######################
+# Testing classes
+#######################
+class ModelingTester( TestCase ):
+    """ 
+        This class provides tests units to the database modeling.
+    """
+
+    def setUp( self ):
+        """
+            Creates the enviroment for the tests.
+        """
+
+
+    def tearDown( self ):
+        """
+            Undo the changes made by the setUp method. 
+        """
+
+
+    def test_integrity_correctness( self ):
+        """
+            Tests the relations of the QA models, ensuring that everything is ok.
+            Testing examples: 
+                - A question cannot have parents. 
+                - A Comment and an Answer cannot have title.
+                - etc
+        """
